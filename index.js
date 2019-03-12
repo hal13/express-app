b946dc7ff5dfe748fed8bd37191ae68c7192ccb1
hal13 <a15z8my@aiit.ac.jp>
2019-03-12 21:07:49 +0900

    add ejs template to index page

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var ejs = require('ejs');
+
+var app = express();
+
+app.engine('ejs', ejs.renderFile);
 
 app.get('/', function (req, res) {
-    res.send('Welcome to Express!')
-})
+    res.render('index.ejs', {
+        title: 'Index',
+        content: 'This is Express-app Top page!'
+    });
+});
 
 app.listen(3000, function () {
-    console.log('Start server port:3000')
-})
+    console.log('Start server port:3000');
+});
