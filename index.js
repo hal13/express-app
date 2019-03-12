0a26becaa005a313b440d4d784c89255e69bac13
hal13 <a15z8my@aiit.ac.jp>
2019-03-12 22:25:07 +0900

    add sub of POST

--- index.js
+++ index.js
@@ -4,33 +4,32 @@ var ejs = require('ejs');
 var app = express();
 
 app.engine('ejs', ejs.renderFile);
-
 app.use(express.static('public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 // ※トップページ
 app.get('/', function (req, res) {
     var msg = 'This is Express Page!<br>'
-        + 'これは、スタイルシートを利用した例です。';
-    var url = '/other?name=taro&pass=yamada';
+        + 'メッセージを書いて送信してください。';
     res.render('index.ejs', {
         title: 'Index', 
         content: msg, 
-        link: {href: url, text: '※別のページに移動'}
     });
 });
 
-// ※otherページ
-app.get('/other', function (req, res) {
-    var name = req.query.name;
-    var pass = req.query.pass;
-    var msg = 'あなたの名前は' + name + 
-        '<br>パスワードは' + pass + 'です。';
+// ※POST送信処理
+app.post('/', function (req, res) {
+    var msg = 'This is Posted Page!<br>' +
+        'あなたは「' + req.body.message +
+        '」と送信しました。';
     res.render('index.ejs', {
-        title: 'Other',
-        content: msg,
-        link: {href: '/', text: '※トップに戻る'}
+        title: 'Posted',
+        content: msg
     });
 })
+
 app.listen(3000, function () {
     console.log('Server is running!');
 });
